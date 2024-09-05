bbba5234bd609bdb6bbf5fbb0386d9c258329beb
guliyevdev <q.behruz1@gmail.com>
2024-09-05 21:02:38 +0400

    chore: Add router and map page

---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import MapPage from "./pages/map";
+
+export const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MapPage />
+    },
+  ]);
