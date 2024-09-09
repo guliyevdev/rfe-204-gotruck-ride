e4417a26b4fbbdd519b690d065489d25caf29161
guliyevdev <q.behruz1@gmail.com>
2024-09-09 10:49:02 +0400

    Update package.json and package-lock.json dependencies

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
+
 import Header from '../components/Header/Header';
 import styles from './page.module.scss';
 
 const containerStyle = {
     width: '100%',
     height: '94vh', // Full screen height
-  };
-  
-  const center = {
+};
+
+const center = {
     lat: 40.4093,  // Example coordinates for Baku
     lng: 49.8671,
-  };
+};
 
 const MapPage: React.FC = () => {
+    const position = {lat: 53.54992, lng: 10.00678};
     return (
         <>
             <Header />
             <div className={styles.app}>
-                <LoadScript googleMapsApiKey="AIzaSyDDuK0Ovh6M1VRvICDiq321wzBwbsT5FSA">
-                    <GoogleMap
-                        mapContainerStyle={containerStyle}
-                        center={center}
-                        zoom={10}
-                    >
-                        {/* You can add markers, info windows, etc. here */}
-                    </GoogleMap>
-                </LoadScript>
-
+                <APIProvider apiKey={'AIzaSyDDuK0Ovh6M1VRvICDiq321wzBwbsT5FSA'}>
+                    <Map defaultCenter={position} defaultZoom={10} mapId='DEMO_MAP_ID'>
+                        <AdvancedMarker position={position} />
+                    </Map>
+                </APIProvider>
                 <div className={styles.sideDiv}>
-                    <h2>Info Panel</h2>
-                    <p>This is a panel with 350px width, padded from the map.</p>
+                    <h2>Yola Davam</h2>
                 </div>
             </div>
         </>
