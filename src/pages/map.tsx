897ccf31ae50b0c44399c53b5fd4fe132d825fab
guliyevdev <q.behruz1@gmail.com>
2024-09-09 18:25:33 +0400

    Update postcss and tailwind configuration files

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,33 +1,30 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
 
 import Header from '../components/Header/Header';
 import styles from './page.module.scss';
+import { Button } from 'antd';
+import { AutocompleteCustom } from '../components/maps/autocomplate';
 
-const containerStyle = {
-    width: '100%',
-    height: '94vh', // Full screen height
-};
-
-const center = {
-    lat: 40.4093,  // Example coordinates for Baku
-    lng: 49.8671,
-};
 
 const MapPage: React.FC = () => {
-    const position = {lat: 53.54992, lng: 10.00678};
+    const position = {lat: 40.409264, lng: 49.867092};
     return (
         <>
             <Header />
             <div className={styles.app}>
-                <APIProvider apiKey={'AIzaSyDDuK0Ovh6M1VRvICDiq321wzBwbsT5FSA'}>
-                    <Map defaultCenter={position} defaultZoom={10} mapId='DEMO_MAP_ID'>
+                <APIProvider apiKey={'AIzaSyD9JBkYu-uZAPoojnbSD_6ZNUm_SGkmpO4'}>
+                    <Map defaultCenter={position} defaultZoom={10} 
+                            gestureHandling={'greedy'}
+                            disableDefaultUI={true}
+                    mapId='DEMO_MAP_ID'>
                         <AdvancedMarker position={position} />
                     </Map>
                 </APIProvider>
                 <div className={styles.sideDiv}>
                     <h2 className='text-2xl font-bold'>Yola Davam</h2>
+                    <AutocompleteCustom onPlaceSelect={onPlaceSelect} />
+                    <Button type="primary">Ant Design Button</Button>
                 </div>
             </div>
         </>
